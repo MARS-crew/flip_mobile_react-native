a7463d1b04d407b7ee7e5c4e95a5525476e1c763
narc2ss <private.narc2ss@gmail.com>
2022-01-12 13:58:25 +0900

    feat/설정화면에 로그아웃 버튼 추가

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from '../components/common/Button';
+import colorPalette from '../theme/colorPalette';
 
 function SettingsScreen() {
-  return <Text>SettingsScreen</Text>;
+  return (
+    <View style={styles.block}>
+      <Button text="로그아웃" onPress={() => {}} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: colorPalette.gray0,
+  },
+});
 
 export default SettingsScreen;
